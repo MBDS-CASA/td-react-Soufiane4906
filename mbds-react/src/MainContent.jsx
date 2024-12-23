d4a3cf50bdbc3645b4284dfff7c9ad70b41b0d27
Soufiane4906 <126627087+Soufiane4906@users.noreply.github.com>
2024-12-23 14:51:04 +0100

    TD02: Afficher la date et l'heure du jour

--- mbds-react/src/MainContent.jsx
+++ mbds-react/src/MainContent.jsx
@@ -1,7 +1,14 @@
+// src/MainContent.jsx
 function MainContent() {
+    const now = new Date();
+    const day = now.toLocaleString('default', { weekday: 'long' });
+    const month = now.toLocaleString('default', { month: 'long' });
+    const year = now.getFullYear();
+    const time = now.toLocaleTimeString();
+
     return (
         <main>
-            <p>Ici, nous afficherons des informations interessantes :)</p>
+            <p>Bonjour, on est le {day}, {month}, {year} et il est {time}</p>
         </main>
     );
 }
