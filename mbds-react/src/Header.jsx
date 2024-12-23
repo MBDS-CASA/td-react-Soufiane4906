f803e8cc8ac33710b9008be2e9b387534d737e16
Soufiane4906 <126627087+Soufiane4906@users.noreply.github.com>
2024-12-23 14:44:54 +0100

    TD01: Ajouter un composant Header dans la page

---
+++ mbds-react/src/Header.jsx
@@ -0,0 +1,11 @@
+import reactLogo from './assets/react.svg';
+
+function Header() {
+    return (
+        <header>
+            <img src={reactLogo} alt="Logo" />
+            <h1>Introduction à React</h1>
+            <h2>A la découverte des premières notions de React</h2>
+        </header>
+    );
+}
